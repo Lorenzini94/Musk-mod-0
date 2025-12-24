a8548b860fa0f11e1f8b5accaabb5d5495e0d79f
Lorenzini <carlos_lorenzini@hotmail.com>
2025-12-24 18:56:40 +0100

    Añadimos al codigo fuente, el año del vehiculo sea coche o moto y agregamos 1 vehiculo de cada tipo

--- Ejercicio.py
+++ Ejercicio.py
@@ -8,23 +8,31 @@ class Vehiculo:
 
 
 class Coche(Vehiculo):
-    def __init__(self, marca, modelo, puertas):
+    def __init__(self, marca, modelo, puertas, anio):
         super().__init__(marca, modelo)
         self.puertas = puertas
+        self.anio = anio
 
     def info(self):
-        print(f"{super().info()}, numero de puertas {self.puertas}")
+        print(f"{super().info()}, numero de puertas {self.puertas}, año {self.anio}")
 
 class Motocicleta(Vehiculo):
-    def __init__(self, marca, modelo, tipo):
+    def __init__(self, marca, modelo, tipo, anio):
         super().__init__(marca, modelo)
         self.tipo = tipo
+        self.anio = anio
 
     def info(self):
-        print(f"{super().info()}, tipo de vehiculo {self.tipo}")
+        print(f"{super().info()}, tipo de vehiculo {self.tipo}, año {self.anio}")
 
-carro1 = Coche("WK", "Gol", 4)
+carro1 = Coche("WK", "Gol", 4, 2020)
 carro1.info()
 
-moto1 = Motocicleta("Kawasaki", "Ninja", "Deportiva")
+moto1 = Motocicleta("Kawasaki", "Ninja", "Deportiva", 2019)
 moto1.info()
+
+carro2 = Coche("Seat", "Leon", 4, 2025)
+carro2.info()
+
+moto2 = Motocicleta("BMW", "1200cc", "Deportiva", 2013)
+moto2.info()
